da5eba73edef00da3f37f2d2466de327d3c8a581
Drew Buschhorn <drewbuschhorn@gmail.com>
2010-11-07 02:15:03 -0600

    Added modifications to send google calendar notifications

--- mymailserver/app/handlers/sample.py
+++ mymailserver/app/handlers/sample.py
@@ -3,24 +3,21 @@ from lamson.routing import route, route_like, stateless
 from config.settings import relay
 from lamson import view
 
+#######
+import sys
+sys.path.append("..")
+sys.path.append("../../gdata-2.0.5/src")
+import simplifiedCalendarExample
+#######
+
 
 @route("(address)@(host)", address=".+")
 def START(message, address=None, host=None):
-    return NEW_USER
+  
+  content_string = "Subject: " + message['Subject']
+  content_string += "\r\n" + message.body()
+  
+  sample = simplifiedCalendarExample.CalendarExample("REPLACEME", "REPLACEME")
+  sample._InsertQuickAddEvent(content_string, message['Subject'])
 
-
-@route_like(START)
-def NEW_USER(message, address=None, host=None):
-    return NEW_USER
-
-
-@route_like(START)
-def END(message, address=None, host=None):
-    return NEW_USER(message, address, host)
-
-
-@route_like(START)
-@stateless
-def FORWARD(message, address=None, host=None):
-    relay.deliver(message)
-
+  return
